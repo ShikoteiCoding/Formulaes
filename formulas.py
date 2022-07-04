f10c0314858b05f8e6f83d74b1a4af910b482ed4
Come Vitis <business.vitis@gmail.com>
2022-07-04 18:34:34 +0200

    Building the amortizing table

--- formulas.py
+++ formulas.py
@@ -22,20 +22,25 @@ def loan_cost_and_payment(amount: int, month_duration: int, yearly_rate: float) 
 
     return payment, payment * month_duration - amount
 
-def _loan_one_month(amount: int, monthly_payment, yearly_rate) -> tuple[float, float, float]:
-
-
-    return 1, 1, 1
-
-def loan_monthly_table(amount: int, month_duration: int, yearly_rate: float) -> list[list]:
+def loan_monthly_table(amount: int, month_duration: int, yearly_rate: float) -> list[tuple[float, float, float, float]]:
     """
     :param amount: total amount of the load
     :param month_duration: duration in month of the credit
     :param yearly_rate: yearly interest rate betweeon 0 and 1 (not percent)
     """
+    monthly_payment, _ = loan_cost_and_payment(amount, month_duration, yearly_rate)
 
-    if month_duration == 0:
-        return []
-    else:
-        balance, interest, principal = _loan_one_month(amount, month_duration, yearly_rate)
-        return loan_monthly_table(amount, month_duration, yearly_rate)
+    monthly_rate = yearly_rate / 12
+
+    def _recursive_loan_monthly(balance: float, table: list) -> list[tuple[float, float, float, float]]:
+        interest = balance * monthly_rate
+        principal = monthly_payment - interest
+        if balance < 0:
+            return table + [(0, monthly_payment, 0, amount)]
+        return _recursive_loan_monthly(
+            balance * (1 + monthly_rate) - monthly_payment,
+            table + [(balance, principal, interest, amount - balance)]
+        )
+
+    return _recursive_loan_monthly(amount, [])
+    
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from formulas import loan_cost_and_payment
+from formulas import loan_cost_and_payment, loan_monthly_table
 
 
 if __name__ == "__main__":
     mensuality, cost = loan_cost_and_payment(100_000, 180, 0.01)
-    print(mensuality)
-    print(cost)
+    table = loan_monthly_table(100_000, 180, 0.01)
