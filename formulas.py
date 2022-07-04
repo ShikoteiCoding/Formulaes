f1bc2bccc3b3dd1f1979fb57ae5ca1a979f2951a
Come Vitis <business.vitis@gmail.com>
2022-07-04 05:10:26 +0200

    Starting the recurrence monthly table

--- formulas.py
+++ formulas.py
@@ -6,8 +6,13 @@ def loan_cost_and_payment(amount: int, month_duration: int, yearly_rate: float) 
     """
     :param amount: total amount of the load
     :param month_duration: duration in month of the credit
-    :param yearly_rate: yearly interest rate
+    :param yearly_rate: yearly interest rate betweeon 0 and 1 (not percent)
     """
+    assert type(amount) == int
+    assert type(month_duration) == int
+    assert type(yearly_rate) == float
+    assert yearly_rate < 1
+    assert yearly_rate >= 0
 
     monthly_rate = yearly_rate / 12
 
@@ -16,3 +21,21 @@ def loan_cost_and_payment(amount: int, month_duration: int, yearly_rate: float) 
     payment = amount*(monthly_rate/denominator)
 
     return payment, payment * month_duration - amount
+
+def _loan_one_month(amount: int, monthly_payment, yearly_rate) -> tuple[float, float, float]:
+
+
+    return 1, 1, 1
+
+def loan_monthly_table(amount: int, month_duration: int, yearly_rate: float) -> list[list]:
+    """
+    :param amount: total amount of the load
+    :param month_duration: duration in month of the credit
+    :param yearly_rate: yearly interest rate betweeon 0 and 1 (not percent)
+    """
+
+    if month_duration == 0:
+        return []
+    else:
+        balance, interest, principal = _loan_one_month(amount, month_duration, yearly_rate)
+        return loan_monthly_table(amount, month_duration, yearly_rate)
--- main.py
+++ main.py
@@ -5,5 +5,3 @@ if __name__ == "__main__":
     mensuality, cost = loan_cost_and_payment(100_000, 180, 0.01)
     print(mensuality)
     print(cost)
-    #print(principal)
-    #print(interests)
