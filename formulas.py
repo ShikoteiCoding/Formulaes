1b381b0e432d7a57cac017b45345a438150c39a1
Come Vitis <business.vitis@gmail.com>
2022-08-12 06:43:30 +0200

    Removing type assertion as they are not useful

--- formulas.py
+++ formulas.py
@@ -1,19 +1,12 @@
 ## Banking
 from calendar import month
 
-
-def loan_cost_and_payment(amount: int, month_duration: int, yearly_rate: float) -> tuple[float, float]:
+def loan_total_cost_and_monthly_payment(amount: int, month_duration: int, yearly_rate: float) -> tuple[float, float]:
     """
     :param amount: total amount of the load
     :param month_duration: duration in month of the credit
     :param yearly_rate: yearly interest rate betweeon 0 and 1 (not percent)
     """
-    assert type(amount) == int
-    assert type(month_duration) == int
-    assert type(yearly_rate) == float
-    assert yearly_rate < 1
-    assert yearly_rate >= 0
-
     monthly_rate = yearly_rate / 12
 
     denominator = (1 - (1 + monthly_rate) ** - month_duration)
@@ -28,7 +21,8 @@ def loan_monthly_table(amount: int, month_duration: int, yearly_rate: float) -> 
     :param month_duration: duration in month of the credit
     :param yearly_rate: yearly interest rate betweeon 0 and 1 (not percent)
     """
-    monthly_payment, _ = loan_cost_and_payment(amount, month_duration, yearly_rate)
+
+    monthly_payment, _ = loan_total_cost_and_monthly_payment(amount, month_duration, yearly_rate)
 
     monthly_rate = yearly_rate / 12
 
@@ -43,4 +37,27 @@ def loan_monthly_table(amount: int, month_duration: int, yearly_rate: float) -> 
         )
 
     return _recursive_loan_monthly(amount, [])
-    
+
+def dividend_investment_table(
+    initial_deposit: int,
+    dividend_yearly_increase: float,
+    stock_expected_growth: float,
+    dividend_yield: float,
+    yearly_investiment: int,
+    holding_duration: int,
+    dividend_tax_rate: float,
+    drip: bool
+    ): 
+    """
+    :param initial_deposit: Initial deposit amount
+    :param dividend_yearly_increase: Dividend year over year increase
+    :param stock_expected_growth: Stock growth
+    :param dividend_yield: Stock yearly returns as a dividend payment
+    :param yearly_investiment: Total invested per year
+    :param holding_duration_year: Total number of year to compute stats
+    :param dividend_tax_rate: Yearly tax rate on "plus-value" returns
+    :param drip: 
+    """
+
+
+    return end_balance, total_return, avg_yearly_return, yearly_dividend_income, total_dividend_payment, yield_on_cost
--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from formulas import loan_cost_and_payment, loan_monthly_table
+from formulas import loan_total_cost_and_monthly_payment, loan_monthly_table
+
+import math
 
 
 if __name__ == "__main__":
-    mensuality, cost = loan_cost_and_payment(100_000, 180, 0.01)
-    table = loan_monthly_table(100_000, 180, 0.01)
+
+    mensuality, cost = loan_total_cost_and_monthly_payment(120_000, 120, 0.07)
+    table = loan_monthly_table(50_000, 180, 0.01)
+    
+    print(mensuality, cost)
