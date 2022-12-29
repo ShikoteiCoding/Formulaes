fde7b75267575da7356422d1bf6bcae009cdf397
Come <56791111+ShikoteiCoding@users.noreply.github.com>
2022-12-29 08:52:50 +0800

    Adding one func to cli

--- formulae/_commands/_banking.py
+++ formulae/_commands/_banking.py
@@ -10,11 +10,11 @@ from formulae.accounting import loan_monthly_table, loan_total_cost_and_monthly_
 def get_loan_total_cost_and_monthly_payment(
     amount: int=100000, month_duration: int=12, yearly_rate: float=0.05
     ) -> int:
-    loan_total_cost_and_monthly_payment(amount=int(amount), month_duration=int(month_duration), yearly_rate=float(yearly_rate))
+    print(loan_total_cost_and_monthly_payment(amount=int(amount), month_duration=int(month_duration), yearly_rate=float(yearly_rate)))
     return 0
 
 
 @register
-def get_loan_monthly_table(args: Any) -> int:
-    loan_monthly_table()
+def get_loan_monthly_table(amount: int, month_duration: int, yearly_rate: float) -> int:
+    print(loan_monthly_table(amount=int(amount), month_duration=int(month_duration), yearly_rate=float(yearly_rate)))
     return 0
